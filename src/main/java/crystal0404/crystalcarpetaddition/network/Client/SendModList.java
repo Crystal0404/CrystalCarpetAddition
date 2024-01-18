8d7671433ad897890ff1a2b96bca0d0fbd358956
Crystal0404 <syh040325@163.com>
2024-01-19 06:17:31 +0800

    大量的"魔法"

--- src/main/java/crystal0404/crystalcarpetaddition/network/Client/SendModList.java
+++ src/main/java/crystal0404/crystalcarpetaddition/network/Client/SendModList.java
@@ -5,13 +5,18 @@ import com.google.gson.GsonBuilder;
 import crystal0404.crystalcarpetaddition.CrystalCarpetAddition;
 import crystal0404.crystalcarpetaddition.network.CCANetwork;
 import crystal0404.crystalcarpetaddition.network.other.ModList;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
+//#if MC >= 12002
+import net.minecraft.client.network.ClientConfigurationNetworkHandler;
+import net.fabricmc.fabric.api.client.networking.v1.ClientConfigurationNetworking;
+//#else
+//$$ import net.minecraft.client.network.ClientPlayNetworkHandler;
+//$$ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+//#endif
 import net.minecraft.network.PacketByteBuf;
 
 import java.util.ArrayList;
@@ -19,7 +24,12 @@ import java.util.Collection;
 
 public class SendModList {
     // 获取并发送模组列表
-    public static void send_mod_list(MinecraftClient client, ClientPlayNetworkHandler handler,
+    public static void send_mod_list(MinecraftClient client,
+                                     //#if MC>= 12002
+                                     ClientConfigurationNetworkHandler handler,
+                                     //#else
+                                     //$$ ClientPlayNetworkHandler handler,
+                                     //#endif
                                      PacketByteBuf buf, PacketSender sender){
         Collection<ModContainer> get = FabricLoader.getInstance().getAllMods();
         Collection<String> get_mod = new ArrayList<>();
@@ -29,9 +39,19 @@ public class SendModList {
         ModList send_mod = new ModList(get_mod);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         buf = PacketByteBufs.create().writeString(gson.toJson(send_mod));
-        if (!ClientPlayNetworking.canSend(CCANetwork.MOD_LIST)){
+        if (
+                //#if MC >= 12002
+                !ClientConfigurationNetworking
+                //#else
+                //$$ !ClientPlayNetworking
+                //#endif
+                        .canSend(CCANetwork.MOD_LIST)){
             CrystalCarpetAddition.LOGGER.warn("The packet sent failed, please try to connect to the server again!");
         }
-        ClientPlayNetworking.send(CCANetwork.MOD_LIST, buf);
+        //#if MC >= 12002
+        ClientConfigurationNetworking.send(CCANetwork.MOD_LIST, buf);
+        //#else
+        //$$ ClientPlayNetworking.send(CCANetwork.MOD_LIST, buf);
+        //#endif
     }
 }
--- src/main/java/crystal0404/crystalcarpetaddition/network/Server/CheckModList.java
+++ src/main/java/crystal0404/crystalcarpetaddition/network/Server/CheckModList.java
@@ -4,13 +4,15 @@ import com.google.gson.Gson;
 import crystal0404.crystalcarpetaddition.CrystalCarpetAddition;
 import crystal0404.crystalcarpetaddition.config.ReadConfig;
 import crystal0404.crystalcarpetaddition.network.other.ModList;
-import lombok.Setter;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.minecraft.network.ClientConnection;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.server.network.ServerPlayerEntity;
+//#if MC < 12002
+//$$ import net.minecraft.server.network.ServerPlayNetworkHandler;
+//$$ import net.minecraft.server.network.ServerPlayerEntity;
+//#else
+import net.minecraft.server.network.ServerConfigurationNetworkHandler;
+//#endif
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
@@ -18,11 +20,14 @@ import net.minecraft.text.TextColor;
 import java.util.Collection;
 
 public class CheckModList {
-    @Setter
-    private static ClientConnection connection;
-    public static void check_mod_list(MinecraftServer server, ServerPlayerEntity player,
-                                      ServerPlayNetworkHandler handler, PacketByteBuf buf,
-                                      PacketSender sender){
+    public static void check_mod_list(MinecraftServer server,
+                                      //#if MC >= 12002
+                                      ServerConfigurationNetworkHandler handler,
+                                      //#else
+                                      //$$ ServerPlayerEntity player,
+                                      //$$ ServerPlayNetworkHandler handler,
+                                      //#endif
+                                      PacketByteBuf buf, PacketSender sender){
         Gson gson = new Gson();
         String get_mod_json = buf.readString();
         Collection<String> mod_list = gson.fromJson(get_mod_json, ModList.class).getMod_list();
@@ -31,11 +36,10 @@ public class CheckModList {
         }
         for (String string : ReadConfig.MOD_BLACK_LIST) {
             if (mod_list.contains(string)){
-                ServerPlayNetworkHandler dis = new ServerPlayNetworkHandler(server, connection, player);
-                dis.disconnect(Text.literal(string).setStyle(Style.EMPTY
-                        .withColor(TextColor.parse("red")))
+                handler.disconnect(Text.literal(string).setStyle(Style.EMPTY
+                        .withColor(TextColor.fromRgb(0xFF5555)))
                         .append(Text.literal(" Cannot be used on this server!")
-                                .setStyle(Style.EMPTY.withColor(TextColor.parse("aqua")))));
+                                .setStyle(Style.EMPTY.withColor(TextColor.fromRgb(0x55FFFF)))));
                 break;
             }
         }
