8c3c850a798169e8c8bda349b0d47c269ccf83d6
Crystal0404 <syh040325@163.com>
2024-02-21 16:27:30 +0800

    Support for 1.20.1

--- src/main/java/crystal0404/crystalcarpetaddition/CCASettings.java
+++ src/main/java/crystal0404/crystalcarpetaddition/CCASettings.java
@@ -7,14 +7,14 @@ public class CCASettings {
     private static final String NETWORK = "Network";
     @Rule(categories = CCA)
     //#if MC >=12002
-    public static boolean MagicBox = false;
+    //$$ public static boolean MagicBox = false;
     //#else
-    //$$ public static boolean MagicBox = true;
+    public static boolean MagicBox = true;
     //#endif
 
     //#if MC < 12004
-    //$$ @Rule(categories = CCA)
-    //$$ public static boolean CEnderPearlChunkLoading = false;
+    @Rule(categories = CCA)
+    public static boolean CEnderPearlChunkLoading = false;
     //#endif
 
     @Rule(categories = CCA)
@@ -22,7 +22,7 @@ public class CCASettings {
     @Rule(categories = NETWORK)
     public static boolean CCAProtocol = false;
     //#if MC >= 12004
-    @Rule(categories = CCA)
-    public static boolean ComparatorCanPlaceAboveAir = false;
+    //$$ @Rule(categories = CCA)
+    //$$ public static boolean ComparatorCanPlaceAboveAir = false;
     //#endif
 }
