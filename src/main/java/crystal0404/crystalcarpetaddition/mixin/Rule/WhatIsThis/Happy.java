177e4dadbc19b550d1a2a1d072360051a39cf7a0
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2024-02-21 20:46:05 +0800

    Support for 1.20.1

--- src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/WhatIsThis/Happy.java
+++ src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/WhatIsThis/Happy.java
@@ -1,12 +1,12 @@
 package crystal0404.crystalcarpetaddition.mixin.Rule.WhatIsThis;
 
-import net.minecraft.client.gui.screen.SplashTextRenderer;
 import net.minecraft.client.resource.SplashTextResourceSupplier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Calendar;
 import java.util.Random;
@@ -14,13 +14,11 @@ import java.util.Random;
 @Mixin(SplashTextResourceSupplier.class)
 public abstract class Happy {
     @Unique
-    Calendar calendar = Calendar.getInstance();
-    @Unique
     Random random = new Random();
-    @Inject(method = "get", at = @At("RETURN"), cancellable = true)
-    private void SplashTextResourceSupplierMixin(CallbackInfoReturnable<SplashTextRenderer> cir){
+    @Inject(method = "get", at = @At("RETURN"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void SplashTextResourceSupplierMixin(CallbackInfoReturnable<String> cir, Calendar calendar){
         if (calendar.get(Calendar.MONTH) == Calendar.MARCH && calendar.get(Calendar.DAY_OF_MONTH) == 25 && random.nextInt(3) == 2){
-            cir.setReturnValue(new SplashTextRenderer("Happy birthday Crystal0404!!!"));
+            cir.setReturnValue("Happy birthday Crystal0404!!!");
         }
     }
 }
