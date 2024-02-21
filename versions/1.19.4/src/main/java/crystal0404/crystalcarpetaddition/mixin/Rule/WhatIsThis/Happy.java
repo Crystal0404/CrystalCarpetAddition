04338dc981f9e8c1fc40465436f7a840bcc34d5f
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2024-02-21 20:49:36 +0800

    Support for 1.20.1

---
+++ versions/1.19.4/src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/WhatIsThis/Happy.java
@@ -0,0 +1,24 @@
+package crystal0404.crystalcarpetaddition.mixin.Rule.WhatIsThis;
+
+import net.minecraft.client.resource.SplashTextResourceSupplier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Calendar;
+import java.util.Random;
+
+@Mixin(SplashTextResourceSupplier.class)
+public abstract class Happy {
+    @Unique
+    Random random = new Random();
+    @Inject(method = "get", at = @At("RETURN"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void SplashTextResourceSupplierMixin(CallbackInfoReturnable<String> cir, Calendar calendar){
+        if (calendar.get(Calendar.MONTH) == Calendar.MARCH && calendar.get(Calendar.DAY_OF_MONTH) == 25 && random.nextInt(3) == 2){
+            cir.setReturnValue("Happy birthday Crystal0404!!!");
+        }
+    }
+}
