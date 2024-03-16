cc492a3ab00a37e71c6a5798850c3f5170f5316b
Crystal0404 <syh040325@163.com>
2024-03-16 22:22:29 +0800

    Migrated some of the spongemixins to mixinextras, to prevent weird problems and complained to me (

--- src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/MagicBox/MagicBoxMixin.java
+++ src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/MagicBox/MagicBoxMixin.java
@@ -20,8 +20,9 @@
 
 package crystal0404.crystalcarpetaddition.mixin.Rule.MagicBox;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import crystal0404.crystalcarpetaddition.CCASettings;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.ScreenHandler;
@@ -29,16 +30,17 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ShulkerBoxBlock.class)
 public abstract class MagicBoxMixin {
-    @Inject(method = "getComparatorOutput", at = @At("HEAD"), cancellable = true)
-    private void ShulkerBoxBlockMixin(BlockState state, World world, BlockPos pos, CallbackInfoReturnable<Integer> cir){
-        if(CCASettings.MagicBox){
-            cir.setReturnValue(ScreenHandler.calculateComparatorOutput((Inventory)world.getBlockEntity(pos)));
-        }
-        cir.setReturnValue(ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos)));
+    @ModifyReturnValue(
+            method = "getComparatorOutput",
+            at = @At("RETURN")
+    )
+    private int getComparatorOutputMixin(int original, @Local(argsOnly = true) World world, @Local(argsOnly = true) BlockPos pos) {
+        // Just in case anyone complains to me, I migrated the code from spongemixin to mixinextras
+        return CCASettings.MagicBox ?
+                ScreenHandler.calculateComparatorOutput((Inventory) world.getBlockEntity(pos)) :
+                ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
     }
 }
--- src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/WhatIsThis/Happy.java
+++ src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/WhatIsThis/Happy.java
@@ -20,13 +20,12 @@
 
 package crystal0404.crystalcarpetaddition.mixin.Rule.WhatIsThis;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.client.gui.screen.SplashTextRenderer;
 import net.minecraft.client.resource.SplashTextResourceSupplier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Calendar;
 import java.util.Random;
@@ -37,10 +36,15 @@ public abstract class Happy {
     private final Calendar calendars = Calendar.getInstance();
     @Unique
     private final Random random = new Random();
-    @Inject(method = "get", at = @At("RETURN"), cancellable = true)
-    private void SplashTextResourceSupplierMixin(CallbackInfoReturnable<SplashTextRenderer> cir){
-        if (calendars.get(Calendar.MONTH) == Calendar.MARCH && calendars.get(Calendar.DAY_OF_MONTH) == 25 && random.nextInt(3) == 2){
-            cir.setReturnValue(new SplashTextRenderer("Happy birthday Crystal0404!!!"));
-        }
+
+    @ModifyReturnValue(
+            method = "get",
+            at = @At("RETURN")
+    )
+    private SplashTextRenderer SplashTextRendererMixin(SplashTextRenderer original) {
+
+        return calendars.get(Calendar.MONTH) == Calendar.MARCH && calendars.get(Calendar.DAY_OF_MONTH) == 25 && random.nextInt(3) == 2 ?
+                new SplashTextRenderer("Happy birthday Crystal0404!!!") :
+                original;
     }
 }
