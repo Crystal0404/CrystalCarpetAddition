c9347ec14c50ab0c64f9beacf55bafa22ec3d9c7
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2024-03-16 21:28:25 +0800

    Modify the return value to avoid people complaining to me all the time

--- src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/NoBatSpawn/NoBatSpawnMixin.java
+++ src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/NoBatSpawn/NoBatSpawnMixin.java
@@ -20,24 +20,20 @@
 
 package crystal0404.crystalcarpetaddition.mixin.Rule.NoBatSpawn;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import crystal0404.crystalcarpetaddition.CCASettings;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.BatEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
-import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BatEntity.class)
 public abstract class NoBatSpawnMixin {
-    @Inject(method = "canSpawn", at = @At("HEAD"), cancellable = true)
-    private static void batCanSpawnMixin(EntityType<BatEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
-        if (CCASettings.NoBatSpawn) {
-            cir.setReturnValue(false);
-        }
+    @ModifyExpressionValue(
+            method = "canSpawn",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldAccess;getSeaLevel()I")
+    )
+    private static int batCanSpawnMixin(int original) {
+        // Modify the return value to avoid people complaining to me all the time (
+        return CCASettings.NoBatSpawn ? Integer.MIN_VALUE : original;
     }
 }
