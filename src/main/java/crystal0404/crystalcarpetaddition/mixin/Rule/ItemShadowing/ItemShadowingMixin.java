daca1e0b37b6a0141bd7d0dd1154ac9dfb7f93b1
Crystal0404 <syh040325@163.com>
2024-03-17 13:56:03 +0800

    Local variable capture is migrated to MixinExtras

--- src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/ItemShadowing/ItemShadowingMixin.java
+++ src/main/java/crystal0404/crystalcarpetaddition/mixin/Rule/ItemShadowing/ItemShadowingMixin.java
@@ -21,11 +21,11 @@
 package crystal0404.crystalcarpetaddition.mixin.Rule.ItemShadowing;
 
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
+import com.llamalad7.mixinextras.sugar.Local;
 import crystal0404.crystalcarpetaddition.CCASettings;
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
@@ -34,7 +34,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Restriction(
         conflict = {
@@ -47,30 +46,30 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 )
 @Mixin(ScreenHandler.class)
 public abstract class ItemShadowingMixin {
-
     @WrapWithCondition(
-            method = "internalOnSlotClick(IILnet/minecraft/screen/slot/SlotActionType;Lnet/minecraft/entity/player/PlayerEntity;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/slot/Slot;setStack(Lnet/minecraft/item/ItemStack;)V", ordinal = 4)
+            method = "internalOnSlotClick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/screen/slot/Slot;setStack(Lnet/minecraft/item/ItemStack;)V",
+                    ordinal = 4
+            )
     )
-    private boolean RemoveMixin(Slot instance, net.minecraft.item.ItemStack stack){
+    private boolean removeMixin(Slot instance, ItemStack stack) {
         return !CCASettings.ItemShadowing;
     }
+
     @Inject(
-            method = "internalOnSlotClick(IILnet/minecraft/screen/slot/SlotActionType;Lnet/minecraft/entity/player/PlayerEntity;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;setStack(ILnet/minecraft/item/ItemStack;)V", ordinal = 1),
-            locals = LocalCapture.CAPTURE_FAILSOFT
+            method = "internalOnSlotClick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/player/PlayerInventory;setStack(ILnet/minecraft/item/ItemStack;)V",
+                    ordinal = 1
+            )
     )
-    //#if MC <= 12001
-    //$$ private void internalOnSlotClickMixin(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci, PlayerInventory playerInventory, Slot slot3, ItemStack itemStack2){
-    //$$     if (CCASettings.ItemShadowing){
-    //$$         slot3.setStack(itemStack2);
-    //$$     }
-    //$$ }
-    //#elseif MC >= 12004
-    private void internalOnSlotClickMixin(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci, PlayerInventory playerInventory, ItemStack itemStack5, Slot slot){
-        if (CCASettings.ItemShadowing){
-            slot.setStack(itemStack5);
+    private void internalOnSlotClickMixin(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci,
+                                          @Local(ordinal = 0) Slot slot, @Local(ordinal = 0) ItemStack itemStack) {
+        if (CCASettings.ItemShadowing) {
+            slot.setStack(itemStack);
         }
     }
-    //#endif
 }
