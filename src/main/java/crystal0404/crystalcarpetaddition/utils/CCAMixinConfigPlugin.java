2b5b3ffc62e3d1290705b87dfaaab3c1410fdd52
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2024-03-31 17:04:18 +0800

    reconstitution

---
+++ src/main/java/crystal0404/crystalcarpetaddition/utils/CCAMixinConfigPlugin.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the Crystal Carpet Addition project, licensed under the
+ * GNU General Public License v3.0
+ *
+ * Copyright (C) 2024  Crystal0404 and contributors
+ *
+ * Crystal Carpet Addition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Crystal Carpet Addition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Crystal Carpet Addition.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package crystal0404.crystalcarpetaddition.utils;
+
+import crystal0404.crystalcarpetaddition.CrystalCarpetAdditionMod;
+import me.fallenbreath.conditionalmixin.api.mixin.RestrictiveMixinConfigPlugin;
+import org.slf4j.Logger;
+
+import java.util.List;
+import java.util.Set;
+
+public class CCAMixinConfigPlugin extends RestrictiveMixinConfigPlugin {
+    @Override
+    protected void onRestrictionCheckFailed(String mixinClassName, String reason) {
+        if (reason.matches(".*%s.*".formatted(mixinClassName))) {
+            CrystalCarpetAdditionMod.LOGGER.warn("[CCA] \"{}\" is disabled because of \"{}\"", mixinClassName, reason);
+        }
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+}
