fa377d00afa4f4ccbd2f92c3d0e6d3c0d6a82e5d
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2024-04-01 19:30:38 +0800

    Some trying

---
+++ versions/1.20.1/src/main/java/crystal0404/crystalcarpetaddition/mixins/rule/MagicBox/ShulkerBoxBlockMixin.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the Crystal Carpet Addition project, licensed under the
+ * GNU General Public License v3.0
+ *
+ * Copyright (C) 2024  Crystal0404 and contributors
+ *
+ * Crystal Carpet Addition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Crystal Carpet Addition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Crystal Carpet Addition.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package crystal0404.crystalcarpetaddition.mixins.rule.MagicBox;
+
+import crystal0404.crystalcarpetaddition.CCASettings;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ShulkerBoxBlock.class)
+public abstract class ShulkerBoxBlockMixin {
+    @Inject(
+            method = "getComparatorOutput",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void getComparatorOutputMixin(BlockState state, World world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+        if (CCASettings.MagicBox) {
+            cir.setReturnValue(ScreenHandler.calculateComparatorOutput((Inventory) world.getBlockEntity(pos)));
+        }
+        cir.setReturnValue(ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos)));
+    }
+}
