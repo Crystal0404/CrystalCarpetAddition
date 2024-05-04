d5d3f558d62cec14fdbaf6e697a9451c6c1d1f57
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2024-05-04 16:48:15 +0800

    Revert "fix MagicBox not work in 1.20.1 and 1.19.4"
    This reverts commit 56796b4f5a7e2f60bcbfa9ececf2289ecce77613.

--- versions/1.20.1/src/main/java/crystal0404/crystalcarpetaddition/mixins/rule/MagicBox/ShulkerBoxBlockMixin.java
+++ versions/1.20.1/src/main/java/crystal0404/crystalcarpetaddition/mixins/rule/MagicBox/ShulkerBoxBlockMixin.java
@@ -20,24 +20,29 @@
 
 package crystal0404.crystalcarpetaddition.mixins.rule.MagicBox;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import crystal0404.crystalcarpetaddition.CCASettings;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ShulkerBoxBlock.class)
 public abstract class ShulkerBoxBlockMixin {
-    @ModifyReturnValue(
+    @Inject(
             method = "getComparatorOutput",
-            at = @At("RETURN")
+            at = @At("HEAD"),
+            cancellable = true
     )
-    private int getComparatorOutputMixin(int original, @Local(argsOnly = true) World world, @Local(argsOnly = true) BlockPos pos) {
-        return CCASettings.MagicBox ?
-                original : ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
+    private void getComparatorOutputMixin(BlockState state, World world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+        if (CCASettings.MagicBox) {
+            cir.setReturnValue(ScreenHandler.calculateComparatorOutput((Inventory) world.getBlockEntity(pos)));
+        }
+        cir.setReturnValue(ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos)));
     }
 }
