1ecd679ca025ca1f7484b5a0f138dd7dafbb408d
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2024-05-24 17:54:47 +0800

    yee?

--- src/main/java/crystal0404/crystalcarpetaddition/mixins/carpet/SettingsManagerMixin.java
+++ src/main/java/crystal0404/crystalcarpetaddition/mixins/carpet/SettingsManagerMixin.java
@@ -20,8 +20,6 @@
 
 package crystal0404.crystalcarpetaddition.mixins.carpet;
 
-import carpet.api.settings.CarpetRule;
-import carpet.api.settings.RuleHelper;
 import carpet.api.settings.SettingsManager;
 import carpet.utils.Messenger;
 import carpet.utils.TranslationKeys;
@@ -32,44 +30,20 @@ import net.minecraft.server.command.ServerCommandSource;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.Objects;
 
 import static carpet.utils.Translations.tr;
 
 @Restriction(require = @Condition("carpet"))  // Maybe something else will be stuffed in the future?
-@Mixin(value = SettingsManager.class, remap = false)
+@Mixin(SettingsManager.class)
 public abstract class SettingsManagerMixin {
-    @Shadow
+    @Shadow(remap = false)
     @Final
     private String fancyName;
-
-    @Shadow
-    @Final
-    private String version;
-
-    @Shadow
-    protected abstract int listSettings(ServerCommandSource source, String title, Collection<CarpetRule<?>> settings_list);
-
-    @Shadow
-    protected abstract Collection<CarpetRule<?>> getNonDefault();
-
-    @Shadow
-    public abstract Iterable<String> getCategories();
-
-    @Shadow
-    public abstract String identifier();
-
-    @Shadow
-    @Final
-    private String identifier;
 
     @Inject(
             method = "listAllSettings",
@@ -77,8 +51,7 @@ public abstract class SettingsManagerMixin {
                     value = "INVOKE",
                     target = "Lcarpet/utils/Messenger;m(Lnet/minecraft/server/command/ServerCommandSource;[Ljava/lang/Object;)V",
                     ordinal = 0,
-                    shift = At.Shift.AFTER,
-                    remap = true
+                    shift = At.Shift.AFTER
             )
     )
     private void listAllSettingsMixin_sendMessage(ServerCommandSource source, CallbackInfoReturnable<Integer> cir) {
@@ -90,44 +63,5 @@ public abstract class SettingsManagerMixin {
             );
             Messenger.m(source, msg);
         }
-    }
-
-    // Redirect to my own clean "listAllSettings"
-    @Inject(
-            method = "listAllSettings",
-            at = @At("HEAD"),
-            cancellable = true,
-            remap = true
-    )
-    private void listAllSettingsMixin(ServerCommandSource source, CallbackInfoReturnable<Integer> cir) {
-        if (Objects.equals(this.fancyName, CrystalCarpetAdditionMod.MOD_NAME)) {
-            cir.setReturnValue(this.CCAListAllSettings(source));
-        }
-    }
-
-    @Unique
-    private int CCAListAllSettings(ServerCommandSource source) {
-        int count = this.listSettings(
-                source, String.format(tr(TranslationKeys.CURRENT_SETTINGS_HEADER), this.fancyName),
-                this.getNonDefault()
-        );
-
-        if (this.version != null)
-            Messenger.m(source, "g " + this.fancyName + " " + tr(TranslationKeys.VERSION) + ": " + version);
-
-        List<String> tags = new ArrayList<>();
-        tags.add("w " + tr(TranslationKeys.BROWSE_CATEGORIES) + ":\n");
-        for (String t : this.getCategories()) {
-            String translated = RuleHelper.translatedCategory(this.identifier(), t);
-            String translatedPlus = !translated.equals(t) ? "%s (%s)".formatted(translated, t) : t;
-            tags.add("c [" + translated + "]");
-            tags.add("^g " + String.format(tr(TranslationKeys.LIST_ALL_CATEGORY), translatedPlus));
-            tags.add("!/" + this.identifier + " list " + t);
-            tags.add("w  ");
-        }
-        tags.remove(tags.size() - 1);
-        Messenger.m(source, tags.toArray(new Object[0]));
-
-        return count;
     }
 }
