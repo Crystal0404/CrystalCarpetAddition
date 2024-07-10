464401c6fd1e054c61a46cca9c89a4b05a1a4dea
Crystal0404 <syh040325@163.com>
2024-07-10 16:38:12 +0800

    fix `ShulkerBoxPowerOutputExpansion` not work when use `MagicBox`

--- versions/1.19.4/src/main/java/crystal0404/crystalcarpetaddition/mixins/rule/MagicBox/ShulkerBoxBlockMixin.java
+++ versions/1.19.4/src/main/java/crystal0404/crystalcarpetaddition/mixins/rule/MagicBox/ShulkerBoxBlockMixin.java
@@ -20,6 +20,8 @@
 
 package crystal0404.crystalcarpetaddition.mixins.rule.MagicBox;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import crystal0404.crystalcarpetaddition.CCASettings;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShulkerBoxBlock;
@@ -27,18 +29,19 @@ import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ShulkerBoxBlock.class)
 public abstract class ShulkerBoxBlockMixin {
-    @Inject(
-            method = "getComparatorOutput",
-            at = @At("HEAD"),
-            cancellable = true
-    )
-    private void getComparatorOutputMixin(BlockState state, World world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
-        if (!CCASettings.MagicBox) cir.setReturnValue(ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos)));
+    @WrapMethod(method = "getComparatorOutput")
+    private int getComparatorOutputMixin(BlockState state, World world, BlockPos pos, Operation<Integer> original) {
+        try {
+            return original.call(state, world, pos);
+        } catch (ClassCastException cce) {
+            if (CCASettings.MagicBox) {
+                throw cce;
+            } else {
+                return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
+            }
+        }
     }
 }
