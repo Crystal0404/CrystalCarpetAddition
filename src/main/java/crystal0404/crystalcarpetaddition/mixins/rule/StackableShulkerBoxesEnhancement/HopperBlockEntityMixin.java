04a168bf90ad25bab8ccfb4921d2955cf7e91314
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2025-01-07 19:43:34 +0800

    new rule `StackableShulkerBoxesEnhancement`

---
+++ src/main/java/crystal0404/crystalcarpetaddition/mixins/rule/StackableShulkerBoxesEnhancement/HopperBlockEntityMixin.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of the Crystal Carpet Addition project, licensed under the
+ * GNU General Public License v3.0
+ *
+ * Copyright (C) 2025  Crystal0404 and contributors
+ *
+ * Crystal Carpet Addition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Crystal Carpet Addition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Crystal Carpet Addition.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package crystal0404.crystalcarpetaddition.mixins.rule.StackableShulkerBoxesEnhancement;
+
+import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import crystal0404.crystalcarpetaddition.CCASettings;
+import crystal0404.crystalcarpetaddition.utils.ModIds;
+import crystal0404.crystalcarpetaddition.utils.shulkerBoxUtils.ShulkerBoxsSet;
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import net.minecraft.block.entity.HopperBlockEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Restriction(conflict = @Condition(ModIds.PCA))
+@Mixin(HopperBlockEntity.class)
+public abstract class HopperBlockEntityMixin {
+    @WrapOperation(
+            method = "transfer(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/inventory/Inventory;" +
+                    "Lnet/minecraft/item/ItemStack;ILnet/minecraft/util/math/Direction;)Lnet/minecraft/item/ItemStack;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/block/entity/HopperBlockEntity;" +
+                            "canMergeItems(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z"
+            )
+    )
+    private static boolean transferMixin(ItemStack first, ItemStack second, Operation<Boolean> original) {
+        if (
+                CCASettings.StackableShulkerBoxesEnhancement
+                        && CarpetSettings.shulkerBoxStackSize != 1
+                        && ShulkerBoxsSet.ITEMS_SET.contains(first.getItem())
+        ) {
+            return false;
+        } else {
+            return original.call(first, second);
+        }
+    }
+}
